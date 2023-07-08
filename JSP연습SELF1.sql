fb32c20b572a1df9d5990e27cfa73e8e340c3f9f
parkdonghoon <daegumian91@gmail.com>
2023-07-08 13:11:57 +0900

    230708

--- JSP연습SELF1.sql
+++ JSP연습SELF1.sql
@@ -1,1 +1,11 @@
-SELECT * FROM USERS2;
+CREATE TABLE USERS (
+    ID VARCHAR2(30),
+    PW VARCHAR2(30) NOT NULL, 
+    NAME VARCHAR2(30) NOT NULL,
+    EMAIL VARCHAR2(30),
+    GENDER CHAR(1),
+    REGDATE DATE DEFAULT SYSDATE
+);
+select * from users;
+
+insert into users(id, pw,name) values(daegumian,1234,¹Ú);
